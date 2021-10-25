62e8e9ab278c33fa84ecee8842e61dcc470b3160
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-10-25 20:53:16 +0700

    Initial commit

---
+++ app/src/main/java/com/android/a6_navigationbottom/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.android.a6_navigationbottom.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
